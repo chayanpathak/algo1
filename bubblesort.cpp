66e5b15be97850a3e112846ccd5095a75a559fef
chayanpathak <chayanpathak2011@gmail.com>
2019-03-01 17:52:09 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -5,7 +5,11 @@ using namespace std;
 void bubblesort(int a[], int n){
 	int temp;
 	for (int i = 0; i < n; i++){
-		for(int j= 0; j < n; j++){
+		cout<<"\n";
+		for(int k = 0; k < n; k++){
+			cout << " "<<a[k];
+		}
+		for(int j= 0; j < n-1; j++){
 			if(a[j] > a[j+1]){
 				temp = a[j];
 				a[j] = a[j+1];
@@ -16,13 +20,14 @@ void bubblesort(int a[], int n){
 } 
 int main() {
 	// your code goes here
-	int a[] = {10,1,2,11,4,57,989,12,13,44,14,6,7,9,8,15,16,17};
+	int a[] = {10,1,2,11,4,57,12,13,44,14,6,7,9,8,15,16,17,98};
 	int arrsize= sizeof(a)/(sizeof(a[0]));
 	int pos;
 	int ele = 7;
 	bubblesort(a,arrsize);
 	for (int i = 0; i < arrsize; i++){
 		cout <<"\n "<<a[i];
+		//cout<<"\n"<<arrsize;
 	}
 	return 0;
 }

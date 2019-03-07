9137500a60d9601ba8f69e20b2e37a970c65f4b5
chayanpathak <chayanpathak2011@gmail.com>
2019-03-07 12:19:01 +0530

    Update heapsort.cpp

--- heapsort.cpp
+++ heapsort.cpp
@@ -7,33 +7,33 @@ void swapper (int first, int second){
     second = first;
     first = temp;
 }
-void heapify (int a[], int size) {
-    for(int i = 0; i < (size/2) ; i++) {
-        int left = 2*i;
-        int right = 2*i + 1;
-        if (a[i] < a[left]) {
-            swap(a[i], a[left]);
+void heapify (int a[], int size, int i) {
+        int largest = i;
+        int left = 2*i + 1;
+        int right = 2*i + 2;
+        if ((a[largest] < a[left]) && (left < size)) {
+            largest = left;
         }
-        else if (a[i] < a[right]) { 
-            swap(a[i],a[right]);
+        if ((a[largest] < a[right]) && (right < size)) { 
+            largest = right;
         }
+        if (largest != i){
+            swap(a[i],a[largest]);
+            heapify(a,size,largest);
+        }
+    for(int j = 0; j < size; j++) {
+        cout <<"\t"<<a[j];
     }
 }
 void heapsort(int a[], int size){
-    int treesize = size;
-    int i = size;
-    heapify(a,size);
-    while (treesize > 0) {
-        cout << " \t " << a[i];
-        swap(a[i],a[0]);
-        i --;
-        treesize--;
-        heapify(a, treesize);
+    for(int i = (size/2)-1; i >= 0; i-- ) {
+        heapify(a,size,i);  
     }
-    cout <<"\n";
-    for(int i = 0; i < size; i++) {
-        cout <<"\t"<<a[i];
+    for(int i = size - 1; i >= 0; i--){
+        swap(a[0],a[i]);
+        heapify(a,i,0);
     }
+
 }
 int main() {
 	// your code goes here

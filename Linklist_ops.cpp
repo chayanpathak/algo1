18fd6225e0942d2a397b891d24024fb8914a8c33
chayanpathak <chayanpathak2011@gmail.com>
2019-04-10 12:41:51 +0530

    Update Linklist_ops.cpp

--- Linklist_ops.cpp
+++ Linklist_ops.cpp
@@ -20,18 +20,5 @@ void link_list_insert(struct Node *head, struct Node *New_node, int position){
 }
 int main() {
 	// your code goes here
-	int a[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
-	int low =0;
-	int high = sizeof(a)/(sizeof(a[0]));
-	cout<<high;
-	int pos;
-	int ele = 7;
-	pos = binsrch(a,low,high,ele);
-	if(pos == -1){
-		cout<<"\n not found";
-	}
-	else {
-		cout <<"\n position of "<<ele<<"  is :"<<pos;
-	}
-	return 0;
+	
 }
